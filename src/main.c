9a507c7317c4306de7089f56a949f74738a04ba2
wizath <kubajot93@gmail.com>
2017-09-12 13:46:38 +0200

    Changed XTAL frequency to 8MHz and pll dividers, added UART printf driver

--- src/main.c
+++ src/main.c
@@ -8,13 +8,39 @@
   ******************************************************************************
 */
 
+#include "stdio.h"
 
 #include "stm32f4xx.h"
 #include "stm32f4xx_gpio.h"
+#include "stm32f4xx_usart.h"
+#include "stm32f4xx_rcc.h"
+
+int __io_putchar(int ch)
+{
+//#ifdef DEBUG
+//	taskENTER_CRITICAL();
+	/* Place your implementation of fputc here */
+	/* e.g. write a character to the USART */
+	USART_SendData(USART1, (uint8_t) ch);
+
+	/* Loop until transmit data register is empty */
+	while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET)
+	{}
+//	taskEXIT_CRITICAL();
+
+	return ch;
+//#endif
+}
 
 int main(void)
 {
+	SystemCoreClockUpdate();
+
 	GPIO_InitTypeDef  GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+//	NVIC_InitTypeDef NVIC_InitStructure;
+
+	// GPIOS LEDS
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 
 	/* Configure PG6 and PG8 in output pushpull mode */
@@ -29,5 +55,39 @@ int main(void)
 	GPIO_SetBits(GPIOC, GPIO_Pin_9);
 	GPIO_SetBits(GPIOC, GPIO_Pin_10);
 
-	for(;;);
+	// USART
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);
+
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+
+	USART_Init(USART1, &USART_InitStructure);
+	USART_Cmd(USART1, ENABLE);
+
+	RCC_ClocksTypeDef RCC_ClockFreq;
+    RCC_GetClocksFreq(&RCC_ClockFreq);
+
+	printf("[log] device boot\n");
+	printf("[log] SYSCLK frequency: %lu\n", RCC_ClockFreq.SYSCLK_Frequency);
+	printf("[log] PCLK1 frequency: %lu\n", RCC_ClockFreq.PCLK1_Frequency);
+	printf("[log] PCLK2 frequency: %lu\n", RCC_ClockFreq.PCLK2_Frequency);
+
+	for(;;)
+	{
+	}
 }
