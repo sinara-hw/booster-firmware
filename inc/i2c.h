e9f0ba0a19e83d0b8394788d160f407c57f289c9
wizath <kubajot93@gmail.com>
2017-09-14 13:12:58 +0200

    Refractored i2c driver

---
+++ inc/i2c.h
@@ -0,0 +1,23 @@
+/*
+ * i2c.h
+ *
+ *  Created on: Sep 14, 2017
+ *      Author: rst
+ */
+
+#ifndef I2C_H_
+#define I2C_H_
+
+#include "config.h"
+
+void init_i2c(void);
+uint8_t i2c_device_connected(I2C_TypeDef* I2Cx, uint8_t address);
+void i2c_write(I2C_TypeDef* I2Cx, uint8_t address, uint8_t reg, uint8_t data);
+uint8_t i2c_read(I2C_TypeDef* I2Cx, uint8_t address, uint8_t reg);
+uint8_t i2c_start(I2C_TypeDef* I2Cx, uint8_t address, uint8_t direction, uint8_t ack);
+uint8_t i2c_stop(I2C_TypeDef* I2Cx);
+uint8_t i2c_write_byte(I2C_TypeDef* I2Cx, uint8_t data);
+uint8_t i2c_read_byte_ack(I2C_TypeDef* I2Cx);
+uint8_t i2c_read_byte_nack(I2C_TypeDef* I2Cx);
+
+#endif /* I2C_H_ */
