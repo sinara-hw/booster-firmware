3da522c6459ac13e0b4ab38a06bc4d08560cd3e8
wizath <kubajot93@gmail.com>
2018-01-22 16:13:10 +0100

    Update v1.1

---
+++ inc/uart.h
@@ -0,0 +1,15 @@
+/*
+ * uart.h
+ *
+ *  Created on: Sep 18, 2017
+ *      Author: rst
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+#include "config.h"
+
+void uart_init(void);
+
+#endif /* UART_H_ */
