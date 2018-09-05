e36238321cf59249a86794a1728acc55c3665189
wizath <kubajot93@gmail.com>
2018-09-05 15:30:28 +0200

    fixed server merge

--- Source/network/server.c
+++ Source/network/server.c
@@ -89,7 +89,6 @@ void prvUDPServerTask(void *pvParameters)
 	uint8_t  destip[4];
 	uint16_t destport;
 	uint8_t rx_buffer[MAX_RX_LENGTH];
-	bool connected = false;
 
 	uint8_t sn = 0;
 
@@ -124,30 +123,28 @@ void prvUDPServerTask(void *pvParameters)
 					switch (getSn_SR(0))
 					{
 						case SOCK_ESTABLISHED:
-							if (!connected) {
-								if(getSn_IR(sn) & Sn_IR_CON)
-								{
-									getSn_DIPR(sn, destip);
-									destport = getSn_DPORT(sn);
-									setSn_IR(sn, Sn_IR_CON);
-									connected = true;
-								}
+							if(getSn_IR(sn) & Sn_IR_CON)
+							{
+								getSn_DIPR(sn, destip);
+								destport = getSn_DPORT(sn);
+								setSn_IR(sn, Sn_IR_CON);
 							}
 
-							len = recv(sn, rx_buffer, MAX_RX_LENGTH);
+							len = getSn_RX_RSR(0);
+							if (len > MAX_RX_LENGTH) len = MAX_RX_LENGTH;
+							recv(sn, rx_buffer, len);
 							rx_buffer[len] = '\0';
 
-//							if (scpi_context.user_context != NULL) {
-//								user_data_t * u = (user_data_t *) (scpi_context.user_context);
-//								memcpy(u->ipsrc, destip, 4);
-//								u->ipsrc_port = destport;
-//							}
+							if (scpi_context.user_context != NULL) {
+								user_data_t * u = (user_data_t *) (scpi_context.user_context);
+								memcpy(u->ipsrc, destip, 4);
+								u->ipsrc_port = destport;
+							}
 
 							SCPI_Input(&scpi_context, (char *) rx_buffer, (int) len);
 							break;
 						case SOCK_CLOSE_WAIT:
 						case SOCK_CLOSED:
-							connected = false;
 							disconnect(sn);
 							socket(0, Sn_MR_TCP, 5000, 0x00);
 							listen(0);
