425be651d7984570087f4309b90e2dd2bf6baf2f
wizath <kubajot93@gmail.com>
2018-10-08 17:12:53 +0200

    calib commands left to rewrite to support new cli

--- Source/board/cli.c
+++ Source/board/cli.c
@@ -53,6 +53,65 @@ static void fh_bootloader(void * a_data)
 }
 
 
+static void fh_int(void * a_data)
+{
+	ucli_ctx_t * a_ctx = (ucli_ctx_t *) a_data;
+	int channel = 0;
+	int value = 0;
+	channel_t * ch;
+
+	ucli_param_get_int(a_ctx, 1, &channel);
+	ucli_param_get_int(a_ctx, 2, &value);
+
+	if ((uint8_t) channel < 8) {
+		ch = rf_channel_get((uint8_t) channel);
+		ch->cal_values.output_dac_cal_value = (uint16_t) value;
+
+		if (ch->enabled) {
+			if (lock_take(I2C_LOCK, portMAX_DELAY))
+			{
+				i2c_mux_select((uint8_t) channel);
+				i2c_dual_dac_set(1, ch->cal_values.output_dac_cal_value);
+				lock_free(I2C_LOCK);
+				printf("[int] Interlock value = %d\r\n", ch->cal_values.output_dac_cal_value);
+			}
+		}
+	} else
+		printf("[int] Wrong channel number\r\n");
+}
+
+
+static void fh_intval(void * a_data)
+{
+	ucli_ctx_t * a_ctx = (ucli_ctx_t *) a_data;
+	int channel = 0;
+	float value = 0;
+	channel_t * ch;
+
+	ucli_param_get_int(a_ctx, 1, &channel);
+	ucli_param_get_float(a_ctx, 2, &value);
+
+	if ((uint8_t) channel < 8) {
+		ch = rf_channel_get(channel);
+		uint16_t dac_value = (uint16_t) (exp((value - ch->cal_values.hw_int_offset) / ch->cal_values.hw_int_scale));
+		printf("Calculated value for pwr %0.2f = %d\n", value, dac_value);
+		ch->cal_values.output_dac_cal_value = dac_value;
+
+		if (lock_take(I2C_LOCK, portMAX_DELAY))
+		{
+			eeprom_write16(DAC2_EEPROM_ADDRESS, dac_value);
+			if (ch->enabled) {
+				i2c_mux_select(channel);
+				i2c_dual_dac_set(1, ch->cal_values.output_dac_cal_value);
+			}
+			lock_free(I2C_LOCK);
+			printf("[intv] Interlock value for %0.2f = %d\r\n", value, ch->cal_values.output_dac_cal_value);
+		}
+	} else
+		printf("[intv] Wrong channel number\r\n");
+}
+
+
 static void fh_enable(void * a_data)
 {
 	ucli_ctx_t * a_ctx = (ucli_ctx_t *) a_data;
@@ -72,6 +131,57 @@ static void fh_disable(void * a_data)
 
 	rf_channels_disable((uint8_t) channel_mask);
 	printf("[disable] Disabled %d channel mask\r\n", (uint8_t) channel_mask);
+}
+
+
+static void fh_clearcal(void * a_data)
+{
+	ucli_ctx_t * a_ctx = (ucli_ctx_t *) a_data;
+	int channel = 0;
+	int value = 0;
+	channel_t * ch;
+
+	ucli_param_get_int(a_ctx, 1, &channel);
+	ucli_param_get_int(a_ctx, 2, &value);
+
+	if ((uint8_t) channel < 8)
+	{
+		ch = rf_channel_get((uint8_t) channel);
+		ch->cal_values.input_dac_cal_value = (uint16_t) value;
+		ch->cal_values.output_dac_cal_value = (uint16_t) value;
+
+		if (lock_take(I2C_LOCK, portMAX_DELAY)) {
+			i2c_mux_select((uint8_t) channel);
+
+			eeprom_write16(DAC1_EEPROM_ADDRESS, (uint16_t) value);
+			vTaskDelay(10);
+			eeprom_write16(DAC2_EEPROM_ADDRESS, (uint16_t) value);
+			lock_free(I2C_LOCK);
+		}
+		printf("[clearcal] DAC values override = %d\r\n", (uint16_t) value);
+	} else
+		printf("[clearcal] Wrong channel number\r\n");
+}
+
+
+static void fh_bias(void * a_data)
+{
+	ucli_ctx_t * a_ctx = (ucli_ctx_t *) a_data;
+	int channel = 0;
+	int value = 0;
+	ucli_param_get_int(a_ctx, 1, &channel);
+	ucli_param_get_int(a_ctx, 2, &value);
+
+	if ((uint8_t) channel < 8) {
+		if (lock_take(I2C_LOCK, portMAX_DELAY))
+		{
+			i2c_mux_select((uint8_t) channel);
+			i2c_dac_set((uint8_t) value);
+			printf("[bias] DAC value = %d\r\n", (uint8_t) value);
+			lock_free(I2C_LOCK);
+		}
+	} else
+		printf("[bias] Wrong channel number\r\n");
 }
 
 
@@ -166,6 +276,7 @@ static void fh_netconfig(void * a_data)
 	}
 }
 
+
 static void fh_macconfig(void * a_data)
 {
 	ucli_ctx_t * a_ctx = (ucli_ctx_t *) a_data;
@@ -187,6 +298,7 @@ static void fh_macconfig(void * a_data)
 	} else
 		printf("[macconfig] Wrong MAC address specified\r\n");
 }
+
 
 void vCommandConsoleTask(void *pvParameters)
 {
@@ -219,6 +331,10 @@ static ucli_cmd_t g_cmds[] = {
 	{ "macconfig", fh_macconfig, 0x01, "Change network MAC address\r\n" },
 	{ "enable", fh_enable, 0x01, "Enable selected channel mask\r\n" },
 	{ "disable", fh_disable, 0x01, "Disable selected channel mask\r\n" },
+	{ "bias", fh_bias, 0x02, "Set channel bias voltage DAC value\r\n" },
+	{ "clearcal", fh_clearcal, 0x02, "Clear hardware interlock calibration values\r\n" },
+	{ "int", fh_int, 0x02, "Set output interlock by raw value\r\n" },
+	{ "intv", fh_intval, 0x02, "Set output interlock by power level\r\n" },
 
     // null
     { 0x00, 0x00, 0x00  }
